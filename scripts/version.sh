2e144f60a8b3d663ac8a6702621e38d91e1af5ff
Yuji Suzuki <y0suzuki0123@gmail.com>
2025-02-16 17:25:48 +0900

    fix(scripts): fix version command

--- scripts/version.sh
+++ scripts/version.sh
@@ -2,5 +2,11 @@
 
 VERSION_SPEC=${1:-patch}
 
-npm version --workspaces --include-workspace-root "$VERSION_SPEC" &&
-  git add package.json packages/*/package.json
+npm version --workspaces --include-workspace-root --no-git-tag-version "$VERSION_SPEC"
+
+git add package.json packages/*/package.json
+
+NEW_TAG="$(node -pe "require('./package.json').version")"
+echo "New tag: $NEW_TAG"
+git commit -m "chore(release): bump version to $NEW_TAG"
+git tag "v$NEW_TAG"
